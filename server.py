839c8111fd3d6d6cdfe9ca1e4dc8f2c7193958f9
jason-n-tran <thismart595@gmail.com>
2023-04-19 22:36:59 -0400

    Implemented query_server function to make get request to server for CPAP pressure updated value which is then displayed in the GUI to replace the existing pressure value every 30 seconds; Added flask handler, driver, and function for CPAP_query route

--- server.py
+++ server.py
@@ -308,6 +308,80 @@ def update_patient(mrn, in_data, date):
     return saved_patient
 
 
+@app.route("/CPAP_query/<mrn>", methods=["GET"])
+def get_pressure_handler(mrn):
+    """
+    GET route to obtain patient's information. This function implements
+    a variable URL in which the server returns patient information. The
+    variable URL will contain the MRN of the patient of interest. This
+    MRN is passed to a driver function that will retrieve the data for
+    this function to return.
+    Parameters
+    ----------
+    patient_mrn : integer or string
+        patient's medical record number
+    Returns
+    -------
+    answer : string
+        Response message indicating type of error
+    status_code : integer
+        200 if successful and 400 if failed
+    """
+    answer, status = get_pressure_driver(mrn)
+    return jsonify(answer), status
+
+
+def get_pressure_driver(mrn):
+    """
+    Implements the 'patient/results/<patient_mrn>' route
+    This function performs the data validation and implementation for the
+    `patient/results/<patient_mrn>` route which retrieves a patient's
+    information. It first validates that the patient MRN exists in the
+    database. It then calls another function to retrieve the patient's
+    information and returns the retrieved dictionary with status code 200
+    Parameters
+    ----------
+    mrn : integer
+        patient medical record number
+    Returns
+    -------
+    string
+        Response message indicating type of error or patient info if no error
+    integer
+        200 if successful and 400 if failed
+    """
+    # Validate input
+    mrn_validation = does_patient_exist_in_db(int(mrn))
+    if mrn_validation is not True:
+        return "Patient not in database", 400
+    # Do the work
+    pressure = get_pressure(int(mrn))
+    # Return an answer
+    return pressure, 200
+
+
+def get_pressure(mrn):
+    """
+    Takes a given patient's medical record number and obtains that patient's
+    open requests and completed test lists from the database. Reformats key
+    for request_date to request_timestamp and returns combined information.
+    Parameters
+    ----------
+    mrn : integer
+        patient medical record number
+    Returns
+    -------
+    dictionary
+        {
+         "patient_mrn": <medical_record_number>,
+         "completed_tests": <list_of_test_dictionaries>,
+         "open requests": <list_of_request_dictionaries>
+        }
+    """
+    x = Patient.objects.raw({"_id": mrn}).first()
+    return x.CPAP_pressure[-1]
+
+
 if __name__ == "__main__":
     init_server()
     app.run()
