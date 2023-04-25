d9998be5888c07f9c5edacc09240c3cbcfe6beae
jason-n-tran <thismart595@gmail.com>
2023-04-24 22:09:51 -0400

    Changed server code for query route to access dictionary; Updated unit tests accordingly; Switched patient class primary key from mrn to room number

--- PatientModel.py
+++ PatientModel.py
@@ -2,8 +2,8 @@ from pymodm import MongoModel, fields
 
 
 class Patient(MongoModel):
-    patient_mrn = fields.IntegerField(primary_key=True)
-    room_number = fields.IntegerField()
+    patient_mrn = fields.IntegerField()
+    room_number = fields.IntegerField(primary_key=True)
     patient_name = fields.CharField()
     CPAP_pressure = fields.ListField()
     breath_rate = fields.ListField()
